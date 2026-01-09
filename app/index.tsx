70fc61c7e5365c3ca73381122ccb51604f321481
ARUJIMU <jhj818949@gmail.com>
2026-01-09 11:41:39 +0800

    update_260109_V:0.0.0.1

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,31 @@
-import { Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>test hollo world.</Text>
+    <View style={styles.container}>
+      <Text style= {styles.form_model}>Edit app/index.tsx to edit this screen.</Text>
+      <Text style= {styles.form_model}>test hollo world.</Text>
+      <StatusBar style="dark" />
+
     </View>
+    
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#aaa',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#000',
+  },
+  form_model:{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+  }
+});
